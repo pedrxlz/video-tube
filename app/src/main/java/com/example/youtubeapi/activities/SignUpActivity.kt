af62914bb2d81f9e808e6bbd52f5b10ff22ed0a5
Pedro Camboim <pedrocamboim@klever.io>
2022-11-27 17:46:09 -0300

    add Firebase authentication

---
+++ app/src/main/java/com/example/youtubeapi/activities/SignUpActivity.kt
@@ -0,0 +1,50 @@
+package com.example.youtubeapi.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.youtubeapi.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener{
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if(it.isSuccessful) {
+                            val intent = Intent(this, SignInActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Senhas diferentes", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Por favor preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
